612ff9c58f7d20b6217416cb68b16775579878e1
Joe Letizia <joe.letizia@gmail.com>
2016-04-10 12:45:18 -0400

    Credit Card Validation

---
+++ baby.hs
@@ -0,0 +1,35 @@
+intListLength :: [Integer] -> Integer
+intListLength []     = 0
+intListLength (x:xs) = 1 + intListLength xs
+
+sumEveryTwo :: [Integer] -> [Integer]
+sumEveryTwo [] = []
+sumEveryTwo (x:[]) = [x]
+sumEveryTwo (x:(y:zs)) = (x + y) : sumEveryTwo zs
+
+revIntList :: [Integer] -> [Integer]
+revIntList [] = []
+revIntList (x) = revIntList(tail(x)) ++ [head(x)]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (x:[]) = [x]
+doubleEveryOther (x:(y:zs)) = x  : y * 2 : doubleEveryOther(zs)
+
+getDigits :: Integer -> [Integer]
+getDigits 0 = []
+getDigits x = getDigits(quot x 10) ++ [x `mod` 10]
+
+mod10 :: Integer -> Integer
+mod10 x = x `mod` 10
+
+decomposeItemsGreaterThan10 :: [Integer] -> [Integer]
+decomposeItemsGreaterThan10 [] = []
+decomposeItemsGreaterThan10 (x:xs) = getDigits(x) ++ decomposeItemsGreaterThan10(xs)
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = x + sumDigits(xs)
+
+validate :: Integer -> Bool
+validate x = (mod10 . sumDigits . decomposeItemsGreaterThan10 . doubleEveryOther . revIntList . getDigits) x == 0
